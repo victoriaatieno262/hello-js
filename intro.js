6b88c803714cd13ee6144c46606ad5360eb305c9
Victoria Ogombe <victoriaatieno262@gmail.com>
2026-08-14 23:39:50 +0300

    Initial commit: Introduction project

---
+++ intro.js
@@ -0,0 +1,7 @@
+const name = "Victoria";
+const age = "19";
+const favouriteLanguage = "Javascript";
+
+console.log("Name: "+ name);
+console.log("Age: ", + age);
+console.log("Favourite Programming Language: " + favouriteLanguage);
